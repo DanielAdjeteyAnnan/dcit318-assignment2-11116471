e61fca9a973c8c904381df9ef8da658583cc5e84
DanielAdjeteyAnnan <daannan001@st.ug.edu.gh>
2025-08-02 01:55:00 +0000

    Completed Interfaces task using IMovable,  Car, Bicycle

--- Program.cs
+++ Program.cs
@@ -1,31 +1,37 @@
 ﻿using System;
 using Inheritance;
 using Abstraction;
+using Interfaces;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Inheritance and Method Overriding
-        Console.WriteLine("Inheritance and Method Overriding ");
-
+        // Inheritance
+        Console.WriteLine(" Inheritance and Method Overriding ");
         Animal genericAnimal = new Animal();
         Animal dog = new Dog();
         Animal cat = new Cat();
-
-        genericAnimal.MakeSound(); // Output: Some generic sound
-        dog.MakeSound();           // Output: Bark
-        cat.MakeSound();           // Output: Meow
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
 
         Console.WriteLine();
 
-        // Abstract Classes and Methods
+        // Abstraction
         Console.WriteLine(" Abstract Classes and Methods ");
-
         Shape circle = new Circle(5.0);
         Shape rectangle = new Rectangle(4.0, 6.0);
-
         Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
         Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+
+        Console.WriteLine();
+
+        // Interfaces
+        Console.WriteLine(" Interfaces ");
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+        car.Move();
+        bicycle.Move();
     }
 }
