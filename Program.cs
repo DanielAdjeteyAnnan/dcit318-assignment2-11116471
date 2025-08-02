c7758e5e752a8f4ec3b375c5e07ef826813134b2
DanielAdjeteyAnnan <daannan001@st.ug.edu.gh>
2025-08-02 01:32:08 +0000

    Completed Inheritance and Method Overriding: Animal, Dog, Cat

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Inheritance and Method Overriding ");
+
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        genericAnimal.MakeSound(); // Output: Some generic sound
+        dog.MakeSound();           // Output: Bark
+        cat.MakeSound();           // Output: Meow
+    }
+}
